4267e50bc6def99893ec29a78c221939af3ba7cb
Theo Gregory <tjmg@outlook.com>
2018-02-20 18:38:52 +0000

    Adds GPU block scan

--- Ex1/src/ex1.cu
+++ Ex1/src/ex1.cu
@@ -35,6 +35,45 @@ if (err != cudaSuccess) {\
 
 #define BLOCK_SIZE 1024
 
+__global__ void
+prescan(int *g_odata, int *g_idata, int n)
+{
+	extern __shared__ int temp[];  // allocated on invocation
+	int thid = threadIdx.x;
+	int offset = 1;
+	temp[2*thid] = g_idata[2*thid]; // load input into shared memory
+	temp[2*thid+1] = g_idata[2*thid+1];
+	for (int d = n>>1; d > 0; d >>= 1)                    // build sum in place up the tree
+	{
+		__syncthreads();
+		if (thid < d)
+		{
+			int ai = offset*(2*thid+1)-1;
+			int bi = offset*(2*thid+2)-1;
+			temp[bi] += temp[ai];
+		}
+		offset *= 2;
+	}
+	if (thid == 0) { temp[n - 1] = 0; } // clear the last element
+	for (int d = 1; d < n; d *= 2) // traverse down tree & build scan
+	{
+		offset >>= 1;
+		__syncthreads();
+		if (thid < d)
+		{
+			int ai = offset*(2*thid+1)-1;
+			int bi = offset*(2*thid+2)-1;
+			int t = temp[ai];
+			temp[ai] = temp[bi];
+			temp[bi] += t;
+		}
+	}
+	__syncthreads();
+	g_odata[2*thid] = temp[2*thid]; // write results to device memory
+	g_odata[2*thid+1] = temp[2*thid+1];
+}
+
+
 int main(void)
 {
 	cudaError_t err = cudaSuccess;
@@ -69,6 +108,7 @@ int main(void)
 		h_host_results[i] = h_host_results[i-1] + h_input_array[i-1];
 	}
 
+	// Check host vectors are as expected
 	printf("%d %d %d %d\n", h_input_array[0], h_input_array[1], h_input_array[2], h_input_array[num_elements-1]);
 	printf("%d %d %d %d\n", h_host_results[0], h_host_results[1], h_host_results[2], h_host_results[num_elements-1]);
 
@@ -85,6 +125,9 @@ int main(void)
 
 
 	// *************************** FSCAN BCAO******************************
+
+
+
 
 	// Clean up the Device timer event objects
 	cudaEventDestroy(start);
